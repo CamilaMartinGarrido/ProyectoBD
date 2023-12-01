0d90d60c3962f81a6dc6e2bf7b8125ac3a754b03
usagi03 <jmspg2017@gmail.com>
2023-11-30 22:05:29 -0500

    Agregado visuales

--- src/visual/controllers/LoginController.java
+++ src/visual/controllers/LoginController.java
@@ -1,26 +1,17 @@
 package visual.controllers;
 
-import com.jfoenix.controls.JFXRippler;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import javafx.animation.FadeTransition;
-import javafx.animation.ScaleTransition;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class LoginController implements Initializable {
 
     @FXML
     private ImageView btnClose;
