c2f10465fe5e00dc05ab8545f2336ffff0008ba5
Adrian <abaez9480@gmail.com>
2023-12-02 22:28:15 -0500

    La clase que hace la conexion con la bd

---
+++ src/conection/DataBaseConnection.java
@@ -0,0 +1,15 @@
+package conection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static java.sql.Connection connection;
+    public DataBaseConnection (String serverAddress, String database, String user,String pass ) throws ClassNotFoundException, SQLException{
+        Class.forName("org.postgresql.Driver");
+        String url =  "jdbc:postgresql://" + serverAddress + ":5432/"+ database;
+        connection = DriverManager.getConnection(url, user, pass);
+    }
+    public java.sql.Connection getConnection() {
+        return connection;
+    }
+}
