dbc3cef4fa410ede04b0e8f1bf1c74a9ddb1c7f2
Adrian <abaez9480@gmail.com>
2023-12-11 10:13:11 -0500

    Nuevas configraciones

--- src/services/ServicesLocator.java
+++ src/services/ServicesLocator.java
@@ -20,6 +20,8 @@ public class ServicesLocator {
     public static Transportation_Model_Service transportation_model_service = null;
     public static TransportationService transportationService = null;
     public static VehicleService vehicleService = null;
+    public static UserService userService = null;
+    public static RoleService roleService = null;
 
     public static java.sql.Connection getConnection() {
         DataBaseConnection connection = null;
@@ -136,5 +138,18 @@ public class ServicesLocator {
             vehicleService = new VehicleService();
         }
         return vehicleService;
+    }
+
+    public static UserService getUserService(){
+        if(userService == null){
+            userService= new UserService();
+        }
+        return userService;
+    }
+    public static RoleService getRoleService(){
+        if(roleService == null){
+            roleService = new RoleService();
+        }
+        return roleService;
     }
 }
